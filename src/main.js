52c9ac0f6207eda5c482d8d6cdc22aaa0dcc965a
vittodan <vittorio.dantico@student.univaq.it>
2024-03-10 17:40:57 +0100

    test of compat flags

--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
-import Vue from 'vue'
-import App from './App.vue'
 
-Vue.config.productionTip = false
+//Vue.config.productionTip = false | DEPRECATO https://v3-migration.vuejs.org/breaking-changes/global-api.html#config-productiontip-removed
 
+//DEPRECATO: Options api sostituita
+/*
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+*/
+
+import { createApp } from 'vue';
+import App from './App.vue';
+import { GLOBAL_MOUNT, INSTANCE_LISTENERS, INSTANCE_EMIT, INSTANCE_SET, COMPILER_IS_ON ,    FILTERS, COMPILER_FILTERS} from '@vue/compat';
+createApp(App, {
+  [GLOBAL_MOUNT]: true,
+  [INSTANCE_LISTENERS]: true,
+  [INSTANCE_EMIT]: true,
+  [INSTANCE_SET]: true,
+  [COMPILER_IS_ON]: true,
+  [FILTERS]: true,
+  [COMPILER_FILTERS]: true,
+}).mount('#app');
